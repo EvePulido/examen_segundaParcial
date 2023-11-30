f2f63473fea42e6588fdd9c1d241b0aed2a10a91
EvePulido <epulido3@ucol.mx>
2023-11-29 21:39:34 -0600

    Primera versión

---
+++ app.js
@@ -0,0 +1,32 @@
+let eventos = ['Charla CSS', 'Charla HTML', 'Charla Javascript', 'Charla Python', 'Charla PHP', 'Charla Java', 'Charla C', 'Charla C++', 'Charla Ruby'];
+let botones = document.querySelectorAll('.boton');
+
+
+function showDom(elemento, arr) {
+    for (let i=0 ; i < arr.length; i++) {
+        document.getElementById(elemento).innerHTML +=
+            `<li>${arr[i]}</li>`;
+    }
+}
+
+showDom ('lista', eventos);
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const botonSeleccionado = boton.textContent;
+        if (botonSeleccionado === "Crear") {
+            let input = document.querySelector('#evento').value;
+            eventos.push(input);
+            limpiarPantalla();
+            showDom ('lista', eventos);
+        } else {
+            eventos.pop();
+            limpiarPantalla();
+            showDom ('lista', eventos);
+        }
+    });
+});
+
+function limpiarPantalla() {
+    document.querySelector('#lista').innerHTML = '';
+}
